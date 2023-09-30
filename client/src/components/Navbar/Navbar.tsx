2c06eeb0d016324bad4aef50eed055431543cd6f
Mark Ding <93517411+Username-is-Illegal@users.noreply.github.com>
2023-09-30 10:51:38 -0400

    Bocchi icon

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -4,26 +4,31 @@ import { useAccountContext } from "../../context";
 import "./Navbar.style.scss";
 
 function Navbar() {
-  const { loggedIn, logout } = useAccountContext();
-  const navigate = useNavigate();
+    const { loggedIn, logout } = useAccountContext();
+    const navigate = useNavigate();
 
-  return (
-    <div className="navbar">
-      <div className="navbar__logo">
-        <Link to="/">Store Logo</Link>
-      </div>
-      <div className="navbar__account">
-        {loggedIn() === false ? (
-          <>
-            <button onClick={() => navigate("/sign-up")}>Sign Up</button>
-            <button onClick={() => navigate("/login")}>Login</button>
-          </>
-        ) : (
-          <button onClick={() => logout()}>Logout</button>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar">
+            <div className="navbar__logo">
+                {/* Linking the image as the logo */}
+                <Link to="/">
+                    <img src="https://pbs.twimg.com/media/FgmWip_XkAEV1Xb?format=jpg&name=small" alt="Store Logo"
+                         width="250"
+                         height="130" />
+                </Link>
+            </div>
+            <div className="navbar__account">
+                {loggedIn() === false ? (
+                    <>
+                        <button onClick={() => navigate("/sign-up")}>Sign Up</button>
+                        <button onClick={() => navigate("/login")}>Login</button>
+                    </>
+                ) : (
+                    <button onClick={() => logout()}>Logout</button>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
